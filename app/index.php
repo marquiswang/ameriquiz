fa8c16a4f16508435bea242b8bf8c8dcc5f5a748
Marquis Wang <marquis@marquiswang.com>
2010-03-14 01:12:17 +0000

    Moved Facebook API initialization code into library file

--- app/index.php
+++ app/index.php
@@ -9,10 +9,7 @@ $smarty->compile_check = true;
 
 // Required stuff to connect to Facebook API
 require_once 'lib/facebook.php';
-
-$appapikey = 'a56164bf95f2ebb8706961860ebb156f';
-$appsecret = '0c4259ab098ed2446706172a23130c6d';
-$facebook = new Facebook($appapikey, $appsecret);
+require_once 'lib/facebook_include.php';
 
 $facebook->require_frame();
 $user_id = $facebook->require_login();
--- app/splashpage.php
+++ app/splashpage.php
@@ -8,10 +8,8 @@ $smarty->compile_check = true;
 
 // Required stuff to connect to Facebook API
 require_once 'lib/facebook.php';
+require_once 'lib/facebook_include.php';
 
-$appapikey = 'a56164bf95f2ebb8706961860ebb156f';
-$appsecret = '0c4259ab098ed2446706172a23130c6d';
-$facebook = new Facebook($appapikey, $appsecret);
 $user_id = $facebook->require_login();
 
 // Add user info to our database
