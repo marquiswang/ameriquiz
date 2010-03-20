b3ec7b48efca570e95d8fcfc774e85ed1ad595c9
Marquis Wang <marquis@marquiswang.com>
2010-03-20 06:45:21 +0000

    load_fb_api.js needs to be unique for each working copy.

--- app/js/load_fb_api.js
+++
@@ -1,4 +0,0 @@
-FB_RequireFeatures(["CanvasUtil", "XFBML"], function(){
-	FB.Facebook.init('29102fb4c455adcdcd6ce0139ac674a3', 'xd_receiver.htm');
-	FB.CanvasClient.startTimerToSizeToContent(); 
-});
