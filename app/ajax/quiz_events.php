d7cdb0c6a42bd93a3dbc785a001ffb44dc5c4184
Marquis Wang <marquis@marquiswang.com>
2010-03-28 23:39:48 +0000

    Fixed quiz_events.php to not ask for columns we're not using for category play as well

--- app/ajax/quiz_events.php
+++ app/ajax/quiz_events.php
@@ -5,16 +5,22 @@ require "../lib/dbconnect.php";
 $num_events = $_GET["numEvents"];
 $category_id = $_GET["category_id"];
 if ($category_id === "null")
-	$query = "SELECT event_id, name, location, year, month, day, increment, longitude, latitude FROM events ORDER BY RAND() LIMIT $num_events";
+	$query = 
+		"SELECT 
+			event_id, name, location, year, month, day, increment, longitude, latitude 
+		FROM events 
+		ORDER BY RAND() 
+		LIMIT $num_events";
 else
-	$query = "SELECT 
-		events.event_id, events.category_id, events.name, events.location, events.datetime, 
-		events.year, events.month, events.day, events.increment, events.longitude, events.latitude 
-	FROM 
-		event_categories 
-	LEFT JOIN events ON event_categories.event_id = events.event_id 
-	LEFT JOIN categories ON categories.category_id = event_categories.category_id 
-	WHERE event_categories.category_id = $category_id";
+	$query = 
+		"SELECT 
+			events.event_id, events.name, events.location, events.year, events.month, events.day, 
+			events.increment, events.longitude, events.latitude 
+		FROM 
+			event_categories 
+		LEFT JOIN events ON event_categories.event_id = events.event_id 
+		LEFT JOIN categories ON categories.category_id = event_categories.category_id 
+		WHERE event_categories.category_id = $category_id";
 
 $result = mysql_query($query);
 
